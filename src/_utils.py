b1cea04101fa3c85ee16e4d88fa55fcf1ba19d9d
Renan Cardoso <renan.cs.sants@gmail.com>
2025-05-01 15:55:59 -0300

    data exploratory step - categorical features

--- src/_utils.py
+++ src/_utils.py
@@ -2,37 +2,47 @@ from scipy.stats import pearsonr
 
 def classificar_correlacao(x, y):
     """
-    Calcula a correlação de Pearson e classifica conforme a tabela fornecida.
+    Calcula a correlação de Pearson e classifica o coeficiente de Pearson conforme a tabela fornecida.
     
-    Parâmetros:
+    Paramters:
     x, y : listas ou arrays numéricos de mesma dimensão.
     
-    Retorno:
+    Return:
     dicionário com:
-        - 'correlacao': valor de Pearson
-        - 'p_valor': valor do p-valor
-        - 'classificacao': descrição qualitativa da correlação
+        - 'p_value': valor do p-valor
+        - 'classif_p_value': descrição qualitativa do p-valor
+        - 'pearson_coef': valor de Pearson
+        - 'classificacao': descrição qualitativa da Correlação
     """
     if len(x) != len(y):
         raise ValueError("As listas devem ter o mesmo tamanho.")
     
+    signif_alpha = 0.05
+
     corr, p_value = pearsonr(x, y)
     
     abs_corr = abs(corr)
+
+    if p_value >= signif_alpha:
+        classif_p_value = "Não há correlação significativa"
+    else:
+        classif_p_value = "Existe correlação significativa"
+
     
     if abs_corr <= 0.19:
-        classificacao = "Correlação bem fraca"
+        classif_pearson = "Correlação bem fraca"
     elif abs_corr <= 0.39:
-        classificacao = "Correlação fraca"
+        classif_pearson = "Correlação fraca"
     elif abs_corr <= 0.69:
-        classificacao = "Correlação moderada"
+        classif_pearson = "Correlação moderada"
     elif abs_corr <= 0.89:
-        classificacao = "Correlação forte"
+        classif_pearson = "Correlação forte"
     else:
-        classificacao = "Correlação muito forte"
+        classif_pearson = "Correlação muito forte"
     
     return {
-        'correlacao': corr,
-        '\np_valor': p_value,
-        '\nclassificacao': classificacao
+        'p-value': float(p_value),
+        'classif_p_value': classif_p_value,
+        'pearson_coef': float(corr),
+        'classificacao': classif_pearson
     }
